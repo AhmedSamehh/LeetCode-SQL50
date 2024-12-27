62b85e556ecd1ad3b52315d4baae9c1e93f6a066
Ahmad Sameh <41856063+AhmedSamehh@users.noreply.github.com>
2024-12-27 18:22:35 +0200

    [585] [Time Beats: 11.32%] - LeetPush

---
+++ 585-Investments-in-2016.sql
@@ -0,0 +1,12 @@
+SELECT ROUND(SUM(tiv_2016)::decimal, 2) as tiv_2016
+FROM Insurance
+WHERE tiv_2015 in (
+    SELECT tiv_2015 FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+)
+AND (lat,lon) in (
+    SELECT lat, lon FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+)
